bae393772276597fb2581a03d5e74dbbe2d2025f
Khizer Hasan Syed <khizerhasans@gmail.com>
2017-02-07 00:57:52 -0800

    Update server.js

--- Construction/back-end/server.js
+++ Construction/back-end/server.js
@@ -2,7 +2,7 @@ var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000,
   mongoose = require('mongoose'),
-  Sensor = require('./api/models/smartIrrigation-sensor-model'),
+ // Sensor = require('./api/models/smartIrrigation-sensor-model'),
   bodyParser = require('body-parser');
   
 mongoose.Promise = global.Promise;
