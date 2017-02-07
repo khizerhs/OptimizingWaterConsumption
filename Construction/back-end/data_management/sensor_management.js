dc563f2681bb3dda5438e1e75c9b80c78963c57d
Khizer Hasan Syed <khizerhasans@gmail.com>
2017-02-07 00:26:12 -0800

    Made changes for better modularity

--- Construction/back-end/data_management/sensor_management.js
+++ Construction/back-end/data_management/sensor_management.js
@@ -1,5 +1,61 @@
+'use strict';
 
 
+var mongoose = require('mongoose'),
+  Sensor = mongoose.model('Sensor');
+
+exports.list_sensors = function(req, res) {
+  Sensor.find({}, function(err, sensor) {
+    if (err)
+      res.send(err);
+    res.json(sensor);
+  });
+};
+
+
+
+
+exports.create_sensor = function(req, res) {
+  var new_sensor = new Sensor(req.body);
+  new_sensor.save(function(err, sensor) {
+    if (err)
+      res.send(err);
+    res.json(sensor);
+  });
+};
+
+
+exports.read_sensor = function(req, res) {
+  Sensor.findById(req.params.Sensor, function(err, sensor) {
+    if (err)
+      res.send(err);
+    res.json(sensor);
+  });
+};
+
+
+exports.update_sensor = function(req, res) {
+  Sensor.findOneAndUpdate(req.params.sensorId, req.body, {new: true}, function(err, sensor) {
+    if (err)
+      res.send(err);
+    res.json(sensor);
+  });
+};
+
+
+exports.delete_sensor = function(req, res) {
+
+
+  Sensor.remove({
+    _id: req.params.sensorId
+  }, function(err, sensor) {
+    if (err)
+      res.send(err);
+    res.json({ message: 'Sensor successfully deleted' });
+  });
+};
+
+/* 
 var schema = require('./schema');
 
 var SensorManagement = schema.Sensor;
@@ -47,4 +103,4 @@ exports.removeSensor = function (req,callback){
         }, function(err) {
             callback(err);
     });
-}
+} */
