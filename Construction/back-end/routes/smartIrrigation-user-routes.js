aef471197a6d66389b51559e64a8c4029d40dcda
Khizer Hasan Syed <khizerhasans@gmail.com>
2017-02-07 18:56:42 -0800

    Added User Management REST API

---
+++ Construction/back-end/routes/smartIrrigation-user-routes.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = function(app) {
+  var smartIrrigation = require('../data_management/user_management');
+
+
+  app.route('/users')
+    .get(smartIrrigation.list_users)
+    .post(smartIrrigation.create_user);
+
+
+  app.route('/users/:userId')
+    .get(smartIrrigation.read_user)
+    .put(smartIrrigation.update_user)
+    .delete(smartIrrigation.delete_user);
+};
