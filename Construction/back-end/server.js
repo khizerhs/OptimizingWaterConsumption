70af857665ef5e2222e58f40ca907f10d59f30cd
Fabrizio Pisciotta <fabriziojps@gmail.com>
2017-04-05 19:00:58 -0700

    addid cors

--- Construction/back-end/server.js
+++ Construction/back-end/server.js
@@ -30,8 +30,9 @@ process.on('uncaughtException', function (err) {
 })
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+	res.header("Access-Control-Allow-Credentials", true);
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
 
     // intercept OPTIONS method
     if ('OPTIONS' == req.method) {
