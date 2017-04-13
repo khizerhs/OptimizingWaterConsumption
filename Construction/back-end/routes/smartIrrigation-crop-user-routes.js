82acfe1d216d0683dad48e30ab95b34ae9aaa386
Fabrizio Pisciotta <fabriziojps@gmail.com>
2017-04-13 12:59:35 -0700

    added crop-user-id api

--- Construction/back-end/routes/smartIrrigation-crop-user-routes.js
+++ Construction/back-end/routes/smartIrrigation-crop-user-routes.js
@@ -1,9 +1,35 @@
 'use strict';
 module.exports = function(app) {
-  var smartIrrigation = require('../data_management/crop_user_management');
+  var cropUserManagement = require('../data_management/crop_user_management');
 
 
   app.route('/crop-user/:userId')
-    .get(smartIrrigation.get_crop_user)
-    .put(smartIrrigation.update_crop_user);
+    .get(cropUserManagement.get_crop_user)
+    .put(cropUserManagement.update_crop_user);
+
+  app.route('/crop-user-id/:crop_user_id')
+  	.get(function(req, res) {
+  		console.log("QUERY"+req.params.crop_user_id)
+		cropUserManagement.getCropUser({_id : req.params.crop_user_id},function(err,cropUser){
+			console.log("Crop"+JSON.stringify(cropUser))
+			if (err)
+                res.status(500).send(err.message);
+			else if(cropUser == undefined || cropUser == null  )
+				res.status(404).json({ message: 'Crop user not found' })
+			else
+				res.status(200).json(cropUser);
+		});
+    })  
+
+  app.route('/crop-user-id/:crop_user_id/mad')
+  	.get(function(req, res) {
+		cropUserManagement.getCropUser({_id : req.params.crop_user_id},function(err,cropUser){
+			if (err)
+                res.status(500).send(err.message);
+			else if(cropUser == undefined || cropUser == null  )
+				res.status(404).json({ message: 'Crop user not found' })
+			else
+				res.status(200).json(cropUser.mad);
+		});
+    })
 };
