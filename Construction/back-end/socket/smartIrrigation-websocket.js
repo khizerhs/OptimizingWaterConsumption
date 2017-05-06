66e9577730ec867c05ab74610f7cc98e68e2ec37
cywu1031 <cywu1031@gmail.com>
2017-05-05 22:10:24 -0700

    update socket

--- Construction/back-end/socket/smartIrrigation-websocket.js
+++ Construction/back-end/socket/smartIrrigation-websocket.js
@@ -1,15 +1,31 @@
 'use strict';
-var ws_clients = [];
-var crop_user = ''
+var ws_clients = {};
 
 exports.start_ws = function(ws) {
     ws.on('connection', function(client) {  
-        console.log('Websocket client connected...');
-        ws_clients.push(client)
-        
-        // server receive
+        console.log('Websocket client connected...');        
+
         client.on('register', function(crop_user) {
+            console.log('register ' + crop_user)
+            if (!(crop_user in ws_clients)) {
+                ws_clients[crop_user] = []
+            }
+            ws_clients[crop_user].push(client)
         });
+
+        client.on('unregister', function(crop_user) {
+            console.log('unregister ' + crop_user)
+            
+            if (crop_user in ws_clients) {
+                for (var i = 0;i < ws_clients[crop_user].length; ++i) {
+                    if (ws_clients[crop_user][i] === client) {
+                        ws_clients[crop_user].splice(i, 1)
+                        break
+                    }
+                }
+            }
+        });
+
 
     });
 };
@@ -20,7 +36,10 @@ exports.emit_ws = function(cuser, message) {
         return
     }
 
-    for (var i = 0; i < ws_clients.length; i++) {
-        ws_clients[i].emit(cuser, message)
+    if (cuser in ws_clients) {
+        for (var i = 0; i < ws_clients[cuser].length; i++) {
+            console.log('emit to ' + cuser + ' ' + message)
+            ws_clients[cuser][i].emit(cuser, message)
+        }
     }
 }
