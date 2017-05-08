7a394ba9f9abef47158184f2cd8c62aa0d74fa2d
shshnkvadali <shshnkvadali@gmail.com>
2017-05-07 17:18:59 -0700

    Api endpoints for crop stage module

--- Construction/back-end/data_management/crop_stage_management.js
+++ Construction/back-end/data_management/crop_stage_management.js
@@ -1,5 +1,45 @@
+var mongoose = require('mongoose'),
+    schema = require('./schema'),
+  CropStage = schema.CropStage;
 
+exports.list_stages = function(req, res) {
+  CropStage.find({crop_id: req.params.cropId}, function(err, stage) {
+    if (stage == undefined || stage == null)
+      res.status(404).json({message: 'Stages Not found'});
+    else if (err)
+      res.send(err);
+    else
+      res.json(stage);
+  });
+};
 
-var schema = require('./schema');
+exports.add_stage = function(req, res) {
+  var new_stage = new CropStage(req.body);
+  new_stage.save(function(err, stage) {
+    if (err)
+      res.send(err);
+    else
+      res.status(201).json(stage);
+  });
+};
 
-var CropStageManagement = schema.CropStage;
+exports.update_stage = function(req,res){
+   CropStage.findOneAndUpdate(req.body._id, req.body, {new: true}, function(err, stage) {
+    if (stage == undefined || stage == null)
+      res.status(404).json({message: 'Stage Not found'});
+    else if (err)
+      res.status(400).json(err);
+    else    
+      res.json(stage);
+  });
+};
+
+exports.delete_stage = function(req, res) {
+CropStage.remove({_id:req.params.cropId}, function(err, stage) {
+    if (err)
+      res.status(400).json(err);
+    else{
+      res.json({ message: 'Stage successfully deleted' });
+    }
+  });
+};
--- Construction/back-end/routes/smartIrrigation-crop-routes.js
+++ Construction/back-end/routes/smartIrrigation-crop-routes.js
@@ -1,6 +1,7 @@
 'use strict';
 module.exports = function(app) {
   var smartIrrigation = require('../data_management/crop_management');
+  var smartIrrigationStage = require('../data_management/crop_stage_management');
 
 
   app.route('/crops')
@@ -11,5 +12,13 @@ module.exports = function(app) {
   app.route('/crops/:cropId')
     .get(smartIrrigation.read_crop)
     .put(smartIrrigation.update_crop)
-    .delete(smartIrrigation.delete_crop);
+    .delete(smartIrrigation.delete_crop)
+
+  app.route('/cropstage/:cropId')
+    .get(smartIrrigationStage.list_stages)
+    .delete(smartIrrigationStage.delete_stage)
+    
+  app.route('/cropstage')
+  .post(smartIrrigationStage.add_stage)
+  .put(smartIrrigationStage.update_stage)
 };
